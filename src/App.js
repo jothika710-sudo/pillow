28d46176bc16b5b33950716588dffb5528f18cbc
jothika710-sudo <jothika710@gmail.com>
2026-07-17 17:07:19 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-function App() {
+import { BrowserRouter, Routes, Route, Link, useParams } from "react-router-dom";
+
+function Home() {
+  return <h1>Most Famous DevOps Tools</h1>;
+}
+
+function Git() {
+  return <h1>I am a Version Control Tool</h1>;
+}
+
+function Jenkins() {
+  return <h1>I am a CI/CD Tool</h1>;
+}
+
+function Docker() {
+  return <h1>I am a Containerization based Tool!</h1>;
+}
+
+function Four() {
+  const { id } = useParams();
+
   return (
     <div>
-      <h1>Most famous DevOps Tools</h1>
+      <h1>I am a Container Orchestration Tool!</h1>
+      <a href="/">Click Here!</a>
+      <h1>Thank You Have a Nice Day!!!</h1>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Git" element={<Git />} />
+        <Route path="/Jenkins" element={<Jenkins />} />
+        <Route path="/Docker" element={<Docker />} />
+        <Route path="/Four/:id" element={<Four />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
